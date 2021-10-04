95fbb005519201341d092726dd685e75c23a8c29
BlackBarto <arnoldogonzalezroman@gmail.com>
2021-10-04 18:23:59 -0400

    Version 1.0

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,13 @@
-alert("HOLAAA")
+let form = document.getElementById("form-Post")
+let title = document.getElementById("title")
+let text_body = document.getElementById("bodyText")
+let postsContainer = document.getElementById("post-container")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let titleFinal = title.value
+    let text_bodyFinal = text_body.value
+    let body = {text: text_bodyFinal, img: false}
+    let publication = new PostCreator(body, titleFinal)
+    PublicationMethod(publication, postsContainer)
+})
